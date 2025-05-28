2f546323cffd5c0ee6adb824b68b0a2e7c55bf9c
novaardiansyah <novaardiansyah78@gmail.com>
2025-05-28 21:26:24 +0700

    MISC. Gradle upgrade

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,7 +1,7 @@
 // env.d.ts
 declare module '@env' {
   export const API_URL: string;
-  export const APP_ENV: string;
+  export const APP_ENV: 'dev' | 'prod';
   export const APP_DEBUG: boolean;
   export const APP_VERSION: string;
 }
