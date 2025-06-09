fd9ec4384f0c80d1e430c872f301a2869f67d315
novaardiansyah <novaardiansyah78@gmail.com>
2025-06-09 15:39:35 +0700

    Update PaymentAccountScreen and add PaymentAccountItem component;

---
+++ src/screens/PaymentAccountScreen/atoms/PaymentAccountItem.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import { CustomTouchableRipple } from '@/components/CustomPaper'
+import { Avatar, List } from 'react-native-paper'
+import { toIndonesianDate } from '@/helpers/UtilsHelper'
+import { ADMIN_URL } from '@env'
+
+interface PaymentAccountItemProps {
+  item: {
+    id: number
+    name: string
+    deposit: number
+    logo: string
+    created_at: Date
+    updated_at: Date
+  }
+}
+
+const PaymentAccountItem = (props: PaymentAccountItemProps) => {
+  const { item } = props
+
+  return (
+    <CustomTouchableRipple onPress={() => {}}>
+      <List.Item
+        title={item.name}
+        description={`${toIndonesianDate(item.updated_at, { weekday: 'short' })}`}
+        descriptionStyle={styles.description}
+        left={props => (
+          <Avatar.Image 
+            {...props} 
+            source={{ uri: `${ADMIN_URL}/storage/${item.logo}` }} 
+            style={styles.listImage}
+            size={40} 
+          />
+        )}
+        right={() => (
+          <View style={styles.listItemRight}>
+            <Text style={styles.depositText}>
+              {`Rp${item.deposit.toLocaleString('id-ID')}`}
+            </Text>
+          </View>
+        )}
+        titleStyle={styles.title}
+      />
+    </CustomTouchableRipple>
+  )
+}
+
+export default PaymentAccountItem
+
+const styles = StyleSheet.create({
+  container: {},
+  listImage: {
+    backgroundColor: 'transparent', 
+    marginLeft: 16
+  },
+  listItemRight: {
+    marginRight: -6,
+    paddingLeft: 10,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 14, 
+    fontWeight: 'bold',
+  },
+  description: {
+    fontSize: 12
+  },
+  depositText: {
+    fontWeight: 'bold', 
+    fontSize: 14,
+    letterSpacing: 0.5,
+    textAlign: 'right',
+    color: '#0F6F3E',
+  },
+})
